67181c1571963d98afaea8e9a439a4099238b6e7
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-16 13:43:37 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
-#include <USB.h>
-#include <USBHIDMouse.h>
+#include "USB.h"
+#include "USBHIDMouse.h"
 
 USBHIDMouse Mouse;
 
-void setup(){
-  Mouse.begin();
+void setup() {
+  // USB初期化
   USB.begin();
+  Mouse.begin();
+
+  // PC側がUSBデバイスを認識するまで少し待つ
+  delay(2000);
 }
 
-void loop(){
-  Mouse.click(MOUSE_LEFT);
+void loop() {
+  // 左クリック押下
+  Mouse.press(MOUSE_LEFT);
+  delay(10);                 // 押している時間
+
+  // 左クリック解放
+  Mouse.release(MOUSE_LEFT);
+  delay(500);                // 次のクリックまでの間隔
 }
